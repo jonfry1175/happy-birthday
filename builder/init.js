596052a949d040c52078343cf5acb25a9aaeed14
jonfry1175 <odiboen@gmail.com>
2024-07-14 22:57:51 +0700

    fix build

--- builder/init.js
+++ builder/init.js
@@ -1,40 +1,70 @@
-const axios = require("axios").default;
-const path = require("path");
-const fs = require("fs");
-const setPic = require("./getPic");
-const genIndex = require("./genIndex");
-const {
-  generateMarkupLocal,
-  generateMarkupRemote,
-} = require("./generateMarkup");
+// const axios = require("axios").default;
+// const path = require("path");
+// const fs = require("fs");
+// const setPic = require("./getPic");
+// const genIndex = require("./genIndex");
+// const {
+//   generateMarkupLocal,
+//   generateMarkupRemote,
+// } = require("./generateMarkup");
 
-require("dotenv").config();
+// require("dotenv").config();
 
-if (!process.env.NAME) throw new Error("Please specify NAME in environment.");
-if (!process.env.PIC) throw new Error("Please specify PIC in environment.");
+// if (!process.env.NAME) throw new Error("Please specify NAME in environment.");
+// if (!process.env.PIC) throw new Error("Please specify PIC in environment.");
 
-const picPath = process.env.PIC;
-const msgPath = process.env.SCROLL_MSG;
+// const picPath = process.env.PIC;
+// const msgPath = process.env.SCROLL_MSG;
 
-//Local initialization
-const setLocalData = async () => {
-  try {
-    const pic = path.join(__dirname, "../local/", picPath);
-    let markup = "";
-    if (msgPath) {
-      const text = fs.readFileSync(path.join(__dirname, "../local/", msgPath), {
-        encoding: "utf-8",
-      });
-      markup = generateMarkupLocal(text);
-    }
-    await setPic(pic);
-    genIndex(markup);
-  } catch (e) {
-    throw new Error(e.message);
-  }
-};
+// //Local initialization
+// const setLocalData = async () => {
+//   try {
+//     const pic = path.join(__dirname, "../local/", picPath);
+//     let markup = "";
+//     if (msgPath) {
+//       const text = fs.readFileSync(path.join(__dirname, "../local/", msgPath), {
+//         encoding: "utf-8",
+//       });
+//       markup = generateMarkupLocal(text);
+//     }
+//     await setPic(pic);
+//     genIndex(markup);
+//   } catch (e) {
+//     throw new Error(e.message);
+//   }
+// };
 
-//Remote initialization
+// //Remote initialization
+// const setRemoteData = async () => {
+//   try {
+//     let res = await axios.get(picPath, {
+//       responseType: "arraybuffer",
+//     });
+//     const pic = res.data;
+//     let markup = "";
+//     if (msgPath) {
+//       const article = msgPath.split("/").pop();
+//       res = await axios.get(
+//         `https://api.telegra.ph/getPage/${article}?return_content=true`
+//       );
+//       const { content } = res.data.result;
+//       markup = content.reduce(
+//         (string, node) => string + generateMarkupRemote(node),
+//         ""
+//       );
+//     }
+//     await setPic(pic);
+//     genIndex(markup);
+//   } catch (e) {
+//     throw new Error(e.message);
+//   }
+// };
+
+// if (process.argv[2] === "--local") setLocalData();
+// else if (process.argv[2] === "--remote") setRemoteData();
+// else console.log("Fetch mode not specified.");
+
+
 const setRemoteData = async () => {
   try {
     let res = await axios.get(picPath, {
@@ -55,11 +85,8 @@ const setRemoteData = async () => {
     }
     await setPic(pic);
     genIndex(markup);
-  } catch (e) {
-    throw new Error(e.message);
+  } catch (error) {
+    console.error("Error fetching remote data:", error.message);
+    throw new Error(error.message); // Rethrow the error for npm-run-all to catch
   }
 };
-
-if (process.argv[2] === "--local") setLocalData();
-else if (process.argv[2] === "--remote") setRemoteData();
-else console.log("Fetch mode not specified.");
